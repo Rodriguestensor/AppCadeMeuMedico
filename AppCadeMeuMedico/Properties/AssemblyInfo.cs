39c930255aba958f4f2160e936f67de0c910510c
Matias <gabrielguesdriro@hotmail.com>
2022-05-13 18:32:07 -0300

    Primeiro Commit

---
+++ AppCadeMeuMedico/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As Informações Gerais sobre um assembly são controladas por meio do 
+// conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("AppCadeMeuMedico")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppCadeMeuMedico")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Configurar o ComVisible como false torna os tipos desse assembly invisíveis 
+// para componentes COM.  Se for necessário acessar um tipo nesse assembly a partir do 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("27d5b590-eb52-4901-9f62-033b51b2b561")]
+
+// As informações de versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou utilizar como padrão os Números de Revisão e da Versão 
+// usando o '*' como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
