597a85b5baff8dae508a2f2a52888d1e1a074efa
Matias <gabrielguesdriro@hotmail.com>
2022-05-17 21:30:31 -0300

    Commit Portela

---
+++ AppCadeMeuMedico/Controllers/MedicosController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AppCadeMeuMedico.Models;
+
+namespace AppCadeMeuMedico.Controllers
+{
+    public class MedicosController : Controller
+    {
+        private EntidadesCadeMeuMedicoBD db = new EntidadesCadeMeuMedicoBD();
+
+       
+
+        public ActionResult Index()
+        {
+            var medicos = db.Medicos.Include(c=>c.Cidades).Include(c=>c.Especialidades).ToList();
+            return View(medicos);
+        }
+        public ActionResult Adicionar()
+        {
+            ViewBag.IDCidade = new SelectList(db.Cidades, "IDCidade", "Nome");
+            ViewBag.IDEspecialidade = new SelectList(db.Especialidades, "IDEspecialidade", "Nome");
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Adicionar(Medicos medico)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                   
+                        db.Medicos.Add(medico);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    
+                
+                
+              
+            }
+
+            ViewBag.IDCidade = new SelectList(db.Cidades, "IDCidade", "Nome", medico.IDCidade);
+            ViewBag.IDEspecialidade = new SelectList(db.Especialidades, "IDEspecialidade", "Nome",medico.IDEspecialidade);
+            return View(medico);
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            var medico = db.Medicos.Find(id);
+            return View("Editar", medico);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Medicos medico)
+        {
+            db.Entry(medico).State = EntityState.Modified;
+            
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //public ActionResult Editar(long id)
+        //{
+
+        //    Medicos medico = db.Medicos.Find(id);
+        //    ViewBag.IDCidade = new SelectList(db.Cidades, "IDCidade", "Nome", medico.IDCidade);
+        //    ViewBag.IDEspecialidade = new SelectList(db.Especialidades, "IDEspecialidade", "Nome", medico.IDEspecialidade);
+        //    return View(medico);
+        //}
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Editar(/*[Bind(Include = "IDMedico,CRM,Nome,Endereco,Bairro,Email,AtendePorConvenio,TemClinica,WebsiteBlog,IDCidade,IDEspecialidade")]*/ Medicos medico, long id)
+        //{
+
+
+        //    if (ModelState.IsValid)
+        //    {
+
+        //        Medicos medicos = db.Medicos.Find(id);
+
+        //        //medicos. = true;
+        //        //db.Entry(medico).State = EntityState.Modified;
+
+
+        //        //db.Entry(medico).State = EntityState.Modified;
+        //        db.Entry(medico).State = EntityState.Detached;
+
+        //        //set the state from the entity that you just received to modified 
+        //        db.Entry(medicos).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        //db.SaveChanges();
+        //        return RedirectToAction("Index");
+
+        //    }
+
+        //    ViewBag.IDCidade = new SelectList(db.Cidades, "IDCidade", "Nome", medico.IDCidade);
+        //    ViewBag.IDEspecialidade = new SelectList(db.Especialidades, "IDEspecialidade", "Nome", medico.IDEspecialidade);
+        //    return View(medico);
+        //}
+
+        //public void Update(Medicos medicos,int id)
+        //{
+        //    //first find the entity to update
+        //    Medicos oldEntity = DbSet.Find(id);
+
+        //    //detach this entity from the DbSet
+        //    Db.Entry(oldEntity).State = EntityState.Detached;
+
+        //    //set the state from the entity that you just received to modified 
+        //    Db.Entry(obj).State = EntityState.Modified;
+        //}
+        [HttpPost]
+        public string Excluir(long id)
+        {
+            try
+            {
+                Medicos medico = db.Medicos.Find(id);
+                db.Medicos.Remove(medico);
+                db.SaveChanges();
+                return Boolean.TrueString;
+            }
+            catch
+            {
+                return Boolean.FalseString;
+            }
+        }
+
+    }
+}
---
+++ AppCadeMeuMedico/Models/MedicoMetadado.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AppCadeMeuMedico.Models
+{
+    [MetadataType(typeof(MedicoMetadado))]
+    public partial class Medico
+    {
+    }
+    public class MedicoMetadado
+    {
+        [Required(ErrorMessage = "Obrigatório informar o CRM")]
+        [StringLength(30, ErrorMessage = "O CRM deve possuirno máximo 30 caracteres")]
+        public string CRM { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Nome")]
+
+        [StringLength(80, ErrorMessage = "O Nome deve possuir nomáximo 80 caracteres")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o Endereço")]
+
+        [StringLength(100, ErrorMessage = "O Endereço deve possuirno máximo 100 caracteres")]
+        public string Endereco { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o Bairro")]
+
+        [StringLength(60, ErrorMessage = "O Bairro deve possuir no máximo 60 caracteres")]
+        public string Bairro { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o E-mail")]
+
+        [StringLength(100, ErrorMessage = "O E-mail deve possuirno máximo 100 caracteres")]
+
+        [EmailAddress(ErrorMessage = "E-mail incorreto")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar se Atende por Convênio")]
+        public bool AtendePorConvenio { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar se Tem Clínica")]
+        public bool TemClinica { get; set; }
+
+        [StringLength(80, ErrorMessage = "O Website deve possuir no máximo 80 caracteres")]
+        //[Url(ErrorMessage = "Website inválido")]
+        public string WebsiteBlog { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a Cidade")]
+       
+        public int IDCidade { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informara Especialidade")]
+        public int IDEspecialidade { get; set; }
+    }
+}
